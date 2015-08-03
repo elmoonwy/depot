ccde7f57f64582933d3912651a28fba99e8c58f0
Eagleforce <elmoonwy@gmail.com>
2015-08-03 15:01:47 -0400

    Add product cache

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'You can only use gif jpg and png images.'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
